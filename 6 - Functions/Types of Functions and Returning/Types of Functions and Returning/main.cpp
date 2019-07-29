8a429e061fa9258f2b3477332bc8c0d98965a6f1
Angel Lim <brian.q.lim@gmail.com>
2019-07-29 08:48:50 -0700

    Finished Func&Ret

--- 6 - Functions/Types of Functions and Returning/Types of Functions and Returning/main.cpp
+++ 6 - Functions/Types of Functions and Returning/Types of Functions and Returning/main.cpp
@@ -8,19 +8,31 @@ std::string returnString();
 
 int main()
 {
+	std::string message;
 	int num1 = 1;
 	int num2;
 
+	//Void is used for displaying messages
 	voidDisplay(num1);
+	std::cout << "\n";
+	
+	//Void can also be used to change the value of variables when it is passed using & (passing by reference)
 	std::cout << "\n";
 	voidCalculate(num1);
 	std::cout << "Number 1 is now: " << num1 << "\n";
-	num2 = returnInt(num1);
+
+	//Return-type functions can return the result of calaculations as opposed to just the value in variables
 	std::cout << "\n";
+	num2 = returnInt(num1);
 	std::cout << "Number 1 is: " << num1 << "\n";
 	std::cout << "Number 2 is: " << num2 << "\n";
-	std::cout << returnString();
 
+	//Return-type functions can be any data type
+	std::cout << "\n";
+	message = returnString();
+	std::cout << "Your message was: " << message;
+
+	std::cout << "\n\n";
 	return 0;
 }
 
@@ -45,8 +57,8 @@ void voidCalculate(int &num1)
 int returnInt(int num1)
 {
 	std::cout << "===returnInt===\n";
-	num1 += 1; 
-	return num1; 
+	std::cout << "num1 will be incremented by 1 and the result will be num2.\n";
+	return ++num1; 
 }
 
 std::string returnString()
@@ -54,8 +66,9 @@ std::string returnString()
 	std::string message;
 	
 	std::cout << "===returnString===\n";
-	std::cout << "Write a message";
-	std::cin >> message;
+	std::cout << "Write a message: ";
+	std::cin.ignore();
+	getline(std::cin, message);
 
 	return message;
 }
